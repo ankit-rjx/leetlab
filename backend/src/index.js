a8143945c1e4f1ce4f5cf79440d949ab909877ac
Ankit Raj <ankitrajcollab@gmail.com>
2025-05-25 23:18:37 +0530

    chore: setup Prisma with initial schema and configuration

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,5 +7,5 @@ dotenv.config();
 const port = 3000;
 
 app.listen(process.env.PORT, () => {
-  console.log(`Express app listening on port ${port}`);
+  console.log(`Express app listening on port ${process.env.PORT}`);
 });
