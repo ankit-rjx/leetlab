1d377279783222175603221351bd39018ecc72f3
Ankit Raj <ankitrajcollab@gmail.com>
2025-05-26 10:19:29 +0530

    feat (bakend): add boilerplate setup for auth routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
 const app = express();
 
 dotenv.config();
 
-const port = 3000;
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello Guys Welcome to leetlab 🔥");
+});
+
+app.use("/api/v1/auth", authRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Express app listening on port ${process.env.PORT}`);
