b89f5089c51aee9ef7e78b82f2cda27feb4b28c2
Ankit Raj <ankitrajcollab@gmail.com>
2025-05-26 11:40:17 +0530

    feat (bakend): add boilerplate setup for auth routes and controllers

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,6 +1,19 @@
 import express from "express";
+import {
+  check,
+  login,
+  logout,
+  register,
+} from "../controller/auth.controller.js";
 
 const authRoutes = express.Router();
 
-export default authRoutes;
+authRoutes.post("/register", register);
 
+authRoutes.post("/login", login);
+
+authRoutes.post("/logout", logout);
+
+authRoutes.get("/check", check);
+
+export default authRoutes;
